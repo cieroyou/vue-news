4c2cd2652ae046ab2294d47c68f03f2b63d057c2
SeraLee <cieroyou1@gmail.com>
2020-04-01 02:58:40 +0900

    AskView, JobsView 각각 데이터를 불러와서 뿌리는 작업을 이전 커밋과 같이 똑같이 진행함

--- src/api/index.js
+++ src/api/index.js
@@ -7,9 +7,9 @@ function fetchNewsList(){
     return axios.get(`${config.baseUrl}news/1.json`);
 }
 function fetchJobsList(){
-
+    return axios.get(`${config.baseUrl}jobs/1.json`);
 }
 function fetchAskList(){
-    
+    return axios.get(`${config.baseUrl}news/1.json`);
 }
-export { fetchNewsList }
+export { fetchNewsList, fetchJobsList, fetchAskList }
--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>Ask</div>
+  <div>
+    <div v-for="item in ask">{{ item.title }}</div>
+  </div>
 </template>
 
 <script>
+import { fetchAskList } from '../api/index'
 export default {
-
+  data(){
+    return{
+      ask:[]
+    }
+  },
+  created(){
+    var vm = this;
+    fetchAskList()
+    .then(function(response){
+      vm.ask = response.data;
+      console.log(vm.ask);
+    })
+    .catch(function(error){
+      console.log(error);
+    })
+  }
 }
 </script>
 
