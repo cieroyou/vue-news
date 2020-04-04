27496c9c4126d0f2c4d13adbea2cb9bd2fc7d057
SeraLee <cieroyou1@gmail.com>
2020-04-05 00:01:03 +0900

    JobsView 스타일링

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -47,7 +47,7 @@ export default {
 
 
 
-<style>
+<style scoped>
 .item-list{
     margin: 0;
     padding: 0;
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,10 +1,26 @@
 <template>
   <div>
-    <div v-for="item in fetchedJobs">
-      <a :href="item.url"> {{ item.title }}</a>
-      <small>{{ item.domain }}</small>
-
-    </div>
+    <ul class="item-list">
+            <!-- 포인트 영역 -->
+            <li v-for="item in fetchedJobs" class="post">
+                <div class="points">
+                    {{item.points}}
+                </div>    
+            <!-- 기타 정보 영역 -->
+            <div>
+                <p class="item-title">
+                    <a :href="item.url"> {{ item.title }}</a>
+                </p>
+                <small class="link-text">
+                    {{ item.time_ago }}
+                    by
+                    {{ item.domain }}
+                </small>
+            </div>
+            <!-- //기타 정보 영역 -->
+           
+            </li>
+        </ul>
   </div>
 </template>
 
