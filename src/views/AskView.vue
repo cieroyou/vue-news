e637515ab60597ba9c06a65731e5b6a487f55c5b
SeraLee <cieroyou1@gmail.com>
2020-04-04 23:52:51 +0900

    AskView 스타일링

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,10 +1,30 @@
 <template>
   <div>
-    <div v-for="item in fetchedAsk">
-      <router-link :to="`/item/${item.id}`">{{ item.title }}</router-link>
-      <small> {{ item.time_ago }} by {{ item.user }} </small>
-    </div>
+    <ul class="item-list">
+            <!-- 포인트 영역 -->
+            <li v-for="item in fetchedAsk" class="post">
+                <div class="points">
+                    {{item.points}}
+                </div>    
+            <!-- 기타 정보 영역 -->
+            <div>
+                <p class="item-title">
+                    <router-link :to="`/item/${item.id}`">
+                      {{ item.title }}
+                    </router-link>
+                </p>
+                <small class="link-text">
+                    {{ item.time_ago }}
+                    by
+                    <router-link :to="`/user/${item.user}`" class="link-text">{{ item.user }}</router-link>
+                </small>
+            </div>
+            <!-- //기타 정보 영역 -->
+           
+            </li>
+        </ul>
   </div>
+  
 </template>
 
 <script>
@@ -25,6 +45,32 @@ export default {
 }
 </script>
 
-<style>
 
+
+<style>
+.item-list{
+    margin: 0;
+    padding: 0;
+}
+
+.post{
+    list-style: none;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #eee;
+}
+.points{
+    width: 80px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #42b883;
+}
+.item-title{
+    margin: 0;
+}
+.link-text{
+    color: #828282;
+}
 </style>
