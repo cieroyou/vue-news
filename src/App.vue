376f10bd85285ff1437aafbf3b1d9ccc5a779ada
SeraLee <cieroyou1@gmail.com>
2020-04-05 20:18:00 +0900

    App.vue 이벤트 버스 코드 버그 수정

--- src/App.vue
+++ src/App.vue
@@ -35,12 +35,12 @@ export default {
     }
   },
   created() {
-    bus.$on('start:spinner', () => this.startSpinner)
-    bus.$on('end:spinner', () => this.endSpinner)
+    bus.$on('start:spinner', this.startSpinner)
+    bus.$on('end:spinner', this.endSpinner)
   },
   beforeDestroy() {
-    bus.$off('start:spinner', () => this.startSpinner);
-    bus.$off('end:spinner', () => this.endSpinner);
+    bus.$off('start:spinner', this.startSpinner);
+    bus.$off('end:spinner', this.endSpinner);
   }
 }
 </script>
